3748cb434e94fa5af3c953c43ae091a86d276ca8
Dylan Roussin <dylanroussin@gmail.com>
2023-01-31 14:07:02 -0500

    click anywhere for JS toggle (+2 points)

--- w04-svg-sample-raw/build/main.js
+++ w04-svg-sample-raw/build/main.js
@@ -1,4 +1,31 @@
-
 // Connect JS vars with SVG id's
-//
 
+const hsObjSVG = document.getElementById("hsSVG");
+
+let svgConDoc; // SVG content document
+let shieldObj; // Shield SVG path
+let handObj; // Hand SVG path
+let svgObj; // SVG object in content document
+
+hsObjSVG.addEventListener("load", function () {
+  // * Put SVG content xml document into JS variable
+  svgConDoc = hsObjSVG.contentDocument;
+
+  // * Put SVG sheild ID in JS variable
+  // shieldObj = svgConDoc.getElementById("shield");
+  // shieldObj.addEventListener("click", toggleHandOpacity);
+
+  svgObj = svgConDoc.getElementById("svgObj");
+  svgObj.addEventListener("click", toggleHandOpacity);
+
+  handObj = svgConDoc.getElementById("hand");
+});
+
+toggleHandOpacity = () => {
+  // alert("toggleHandOpacity called");
+  if (handObj.style.opacity == 0.5) {
+    handObj.style.opacity = 1;
+  } else {
+    handObj.style.opacity = 0.5;
+  }
+};
