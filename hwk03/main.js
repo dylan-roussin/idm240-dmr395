1d59fb156955b8f40b84d2d107bc772a967f9b8d
Dylan Roussin <dylanroussin@gmail.com>
2023-02-07 12:29:31 -0500

    animations

--- hwk03/main.js
+++ hwk03/main.js
@@ -7,7 +7,8 @@ const ampSVG = document.getElementById("ampSVG");
 const doughnutPinkSVG = document.getElementById("doughnutPinkSVG");
 
 // specific paths
-let brownObj;
+let brownSvgObj;
+let brownSprinklesObj
 let textObj;
 let ampObj;
 let pinkObj;
@@ -26,8 +27,17 @@ doughnutBrownSVG.addEventListener("load", function () {
   svgObj.addEventListener("click", handleBrown);
 
   // Define paths to interact with JS
-  brownSvgObj = svgConDoc.getElementById("brownSvgObj");
-  brownSprinklesObj = svgConDoc.getElementsByClassName('sprinkles');
+  brownSvgObj = svgConDoc.getElementById("brownFill");
+  brownSprinklesObj = svgConDoc.querySelector('#sprinkles0');
+  s1 = svgConDoc.querySelector('#sprinkles1');
+  s2 = svgConDoc.querySelector('#sprinkles2');
+  s3 = svgConDoc.querySelector('#sprinkles3');
+  s4 = svgConDoc.querySelector('#sprinkles4');
+  s5 = svgConDoc.querySelector('#sprinkles5');
+  s6 = svgConDoc.querySelector('#sprinkles6');
+  s7 = svgConDoc.querySelector('#sprinkles7');
+  s8 = svgConDoc.querySelector('#sprinkles8');
+  s9 = svgConDoc.querySelector('#sprinkles9');
 });
 
 // * Baker's dozen text
@@ -66,7 +76,7 @@ doughnutPinkSVG.addEventListener("load", function () {
   svgObj.addEventListener("click", handlePink);
 
   // Define paths to interact with JS
-  pinkObj = svgConDoc.getElementById("Path_804");
+  pinkObj = svgConDoc.getElementById("Path_807");
 });
 
 //
@@ -75,36 +85,53 @@ doughnutPinkSVG.addEventListener("load", function () {
 
 handleBrown = () => {
   // alert("handleBrown called");
-  brownSvgObj.classList.remove('pop'); // reset animation
-  // brownSprinklesObj.classList.remove('popper');
-  void brownSvgObj.offsetWidth; // trigger reflow
-  brownSvgObj.classList.add('pop');
-  // brownSprinklesObj.classList.add('popper');
+  if (brownSvgObj && brownSprinklesObj) {
+    brownSvgObj.classList.remove("pop");
+    brownSprinklesObj.classList.remove("popper");
+    s1.classList.remove("popper");
+    s2.classList.remove("popper");
+    s3.classList.remove("popper");
+    s4.classList.remove("popper");
+    s5.classList.remove("popper");
+    s6.classList.remove("popper");
+    s7.classList.remove("popper");
+    s8.classList.remove("popper");
+    s9.classList.remove("popper");
+  }
+  setTimeout(() => {
+    brownSvgObj.classList.add('pop');
+    brownSprinklesObj.classList.add('popper');
+    s1.classList.toggle("popper");
+    s2.classList.toggle("popper");
+    s3.classList.toggle("popper");
+    s4.classList.toggle("popper");
+    s5.classList.toggle("popper");
+    s6.classList.toggle("popper");
+    s7.classList.toggle("popper");
+    s8.classList.toggle("popper");
+    s9.classList.toggle("popper");
+  }, 100);
 };
+
 
 handleText = () => {
   // alert("handleText called");
-  if (textObj.style.opacity == 0.5) {
-    textObj.style.opacity = 1;
-  } else {
-    textObj.style.opacity = 0.5;
-  }
+  textSVG.contentDocument.getElementById("textSvgObj").classList.remove("spin");
+  setTimeout(() => {
+    textSVG.contentDocument.getElementById("textSvgObj").classList.add("spin");
+  }, 100);
 };
 
 handleAmp = () => {
   // alert("handleAmp called");
-  if (ampObj.style.opacity == 0.5) {
-    ampObj.style.opacity = 1;
-  } else {
-    ampObj.style.opacity = 0.5;
-  }
+  ampSVG.contentDocument.getElementById("ampSvgObj").classList.remove("scaleme");
+  setTimeout(() => {
+    ampSVG.contentDocument.getElementById("ampSvgObj").classList.add("scaleme");
+  }, 100);
 };
 
 handlePink = () => {
   // alert("handlePink called");
-  if (pinkObj.style.opacity == 0.5) {
-    pinkObj.style.opacity = 1;
-  } else {
-    pinkObj.style.opacity = 0.5;
-  }
+  pinkObj.classList.toggle("yellow");
 };
+
