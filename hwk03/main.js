d985016de67ab3996d9a70e34a7a8e40376d80df
Dylan <71848660+dylan-roussin@users.noreply.github.com>
2023-02-06 22:52:56 -0500

    event handlers

--- hwk03/main.js
+++ hwk03/main.js
@@ -1,31 +1,106 @@
-// Connect JS vars with SVG id's
+//
+// * Connect JS vars with SVG id's
+//
 
 const doughnutBrownSVG = document.getElementById("doughnutBrownSVG");
 const textSVG = document.getElementById("textSVG");
 const ampSVG = document.getElementById("ampSVG");
 const doughnutPinkSVG = document.getElementById("doughnutPinkSVG");
 
-let svgConDoc; // SVG content document
-let shieldObj; // Shield SVG path
-let handObj; // Hand SVG path
-let svgObj; // SVG object in content document
+// specific paths
+let brownObj;
+let textObj;
+let ampObj;
+let pinkObj;
 
+//
+// * addEvent listeners to every SVG
+//
+
+// * Brown doughnut
 doughnutBrownSVG.addEventListener("load", function () {
-  // * Put SVG content xml document into JS variable
-  svgConDoc = doughnutBrownSVG.contentDocument;
+  // Put SVG content xml document into JS variable
+  const svgConDoc = doughnutBrownSVG.contentDocument;
 
-  // * Put SVG shield ID in JS variable
-  svgObj = svgConDoc.getElementById("brownSvgObj");
-  svgObj.addEventListener("click", toggleHandOpacity);
+  // Put SVG ID in JS variable
+  const svgObj = svgConDoc.getElementById("brownSvgObj");
+  svgObj.addEventListener("click", handleBrown);
 
-  handObj = svgConDoc.getElementById("hand");
+  brownObj = svgConDoc.getElementById("hand2");
 });
 
-toggleHandOpacity = () => {
-  // alert("toggleHandOpacity called");
+// * Baker's dozen text
+textSVG.addEventListener("load", function () {
+  // Put SVG content xml document into JS variable
+  const svgConDoc = textSVG.contentDocument;
+
+  // Put SVG ID in JS variable
+  const svgObj = svgConDoc.getElementById("textSvgObj");
+  svgObj.addEventListener("click", handleText);
+
+  textObj = svgConDoc.getElementById("hand");
+});
+
+// * Guitar amp
+ampSVG.addEventListener("load", function () {
+  // Put SVG content xml document into JS variable
+  const svgConDoc = ampSVG.contentDocument;
+
+  // Put SVG ID in JS variable
+  const svgObj = svgConDoc.getElementById("ampSvgObj");
+  svgObj.addEventListener("click", handleAmp);
+
+  ampObj = svgConDoc.getElementById("hand");
+});
+
+// * Pink doughnut
+doughnutPinkSVG.addEventListener("load", function () {
+  // Put SVG content xml document into JS variable
+  const svgConDoc = doughnutPinkSVG.contentDocument;
+
+  // Put SVG ID in JS variable
+  const svgObj = svgConDoc.getElementById("pinkSvgObj");
+  svgObj.addEventListener("click", handlePink);
+
+  pinkObj = svgConDoc.getElementById("Path_804");
+});
+
+//
+// * define micro-interactions
+//
+
+handleBrown = () => {
+  alert("handleBrown called");
   if (handObj.style.opacity == 0.5) {
     handObj.style.opacity = 1;
   } else {
     handObj.style.opacity = 0.5;
+  }
+};
+
+handleText = () => {
+  alert("handleText called");
+  if (hand2Obj.style.opacity == 0.5) {
+    hand2Obj.style.opacity = 1;
+  } else {
+    hand2Obj.style.opacity = 0.5;
+  }
+};
+
+handleAmp = () => {
+  alert("handleAmp called");
+  if (hand2Obj.style.opacity == 0.5) {
+    hand2Obj.style.opacity = 1;
+  } else {
+    hand2Obj.style.opacity = 0.5;
+  }
+};
+
+handlePink = () => {
+  alert("handlePink called");
+  if (hand2Obj.style.opacity == 0.5) {
+    hand2Obj.style.opacity = 1;
+  } else {
+    hand2Obj.style.opacity = 0.5;
   }
 };
